85807f9ccce23fb218cdc6479ce4c28774f63493
happikin <raina@fev.com>
2022-12-28 16:18:26 +0530

    added naming corrections and readme

--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
+#### Overview
+1.  This is a thread safe container based on ring buffer data structure
+2.  Thread safety has been acheived using std::atomic_flag to simulate the use of a mutex
+3.  Best part of the ring_buffer is its ability to take in size and type as template argument
+
 #### How To Build
-1.  `mkdir build`
-2.  `cmake ..`
-3.  `make -j$(nproc)`
+    mkdir build
+    cmake ..
+    make
--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <thread>
-#include "ringbuffer.hpp"
+#include <ring_buffer.hpp>
 
 struct data_s {};
 
 int main(int argc, char const *argv[]) {
     sdv::ring_buffer<data_s*,1'000'000> buffer;
+
     std::thread t1([&](){
         for(int i{};i<500'000;i++) {
             data_s *d = new data_s;
@@ -13,6 +14,7 @@ int main(int argc, char const *argv[]) {
             d = nullptr;
         }
     });
+
     std::thread t2([&](){
         for(int i{};i<500'000;i++) {
             data_s *d = new data_s;
@@ -20,6 +22,7 @@ int main(int argc, char const *argv[]) {
             d = nullptr;
         }
     });
+
     std::thread t3([&](){
         for(int i{};i<500'000;i++) {
             data_s *d = new data_s;
@@ -27,6 +30,7 @@ int main(int argc, char const *argv[]) {
             d = nullptr;
         }
     });
+
     std::thread t4([&](){
         for(int i{};i<500'000;i++) {
             data_s *d = new data_s;
@@ -34,8 +38,10 @@ int main(int argc, char const *argv[]) {
             d = nullptr;
         }
     });
+
     t1.join(); t2.join(); t3.join(); t4.join();
     std::cout << "received packets  : " << buffer.size() << "\n";
     std::cout << "lost packets      : " << buffer.lost_packets << "\n";
+    
     return 0;
 }
